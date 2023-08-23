dcf39ae147eb467ff6db2cfc194178fca32b6f72
shirinrezamedha <109938801+shirinrezamedha@users.noreply.github.com>
2023-08-24 00:33:19 +0600

    Delete Lab 5 directory

--- Lab 5/controller/delete_product.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-require_once '../model/model.php';
-
-if (deleteproduct($_GET['id'])) {
-    header('Location: ../show_all_product.php');
-}
-
- ?>
--- Lab 5/nav.php
+++
@@ -1,6 +0,0 @@
- <ul>
-        <li><a href="Addproduct.php"> Add Product</a></li>
-        <li><a href="show_all_product.php"> Show all Products</a></li>
-        <li><a href="search_product.php"> Search a Product</a></li> 
-
-    </ul>
